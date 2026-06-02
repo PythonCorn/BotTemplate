2a6de21e773187e380612fd6653fd0c3c7641007
compocarno <belyankiss@gmail.com>
2026-06-02 20:15:08 +0300

    Organize imports in env.py

--- alembic/env.py
+++ alembic/env.py
@@ -1,21 +1,16 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from sqlalchemy import engine_from_config, pool
 
 from alembic import context
-
-from app.database.base import Base
 from app.core.config import settings
+from app.database.base import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
-config.set_main_option(
-    "sqlalchemy.url",
-    settings.POSTGRES_URI.replace("+asyncpg", "+psycopg2")
-)
+config.set_main_option("sqlalchemy.url", settings.POSTGRES_URI.replace("+asyncpg", "+psycopg2"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -72,9 +67,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
